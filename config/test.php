70a7c08fbe4239b10d98ded3cd839a33cae76bd7
lgy <18612192370@163.com>
2018-12-19 19:25:42 +0800

    test

--- config/test.php
+++ config/test.php
@@ -1,9 +1,9 @@
 <?php
 return [
 
-    'client_id' => 'YXA6nJYNkDe0EeeTrQe-0RzIIg',
-    'client_secret' => 'YXA6CjjCkxGmbytLbMbeV1wzTXp7LeY',
-    'org_name' => '1157170513115158',
-    'app_name' => 'musicwave',
+    'client_id' => '',
+    'client_secret' => '',
+    'org_name' => '',
+    'app_name' => '',
 
 ];
