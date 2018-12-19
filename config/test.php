43d4ca4757e367c7598fc1fcb5207eaced8f7f4d
lgy <18612192370@163.com>
2018-12-19 18:44:16 +0800

    config

--- config/test.php
+++ config/test.php
@@ -1,9 +1,9 @@
 <?php
-
 return [
 
-	'key' => 123,
-	'token' => 123123123123123
+    'client_id' => 'YXA6nJYNkDe0EeeTrQe-0RzIIg',
+    'client_secret' => 'YXA6CjjCkxGmbytLbMbeV1wzTXp7LeY',
+    'org_name' => '1157170513115158',
+    'app_name' => 'musicwave',
 
 ];
-
